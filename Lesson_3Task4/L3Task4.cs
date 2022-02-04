241caf0d32eb58c7ab76196bf12e131970e872a4
AtomMushroom <35073893+AtomMushroom@users.noreply.github.com>
2022-02-04 18:05:31 +0300

    Add files via upload

---
+++ Lesson_3Task4/L3Task4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_3Task3
+{
+    class L3Task4
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int counter = 0; counter <= 9; counter++)
+            {
+                int number = Int32.Parse(Console.ReadLine());
+                sum += number;
+            }
+            Console.WriteLine("Сумма чисел: " + sum);
+        }
+    }
+}
---
+++ Lesson_3Task5/L3Task5.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson_3Task3
+{
+    class L3Task4
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int counter = 0; counter <= 20; counter++)
+            {
+                if (counter % 2 != 0)
+                {
+                    sum += counter;
+                    Console.WriteLine(counter);
+                }
+            }
+            Console.WriteLine("Сумма чисел последовательности: " + sum);
+        }
+    }
+}
